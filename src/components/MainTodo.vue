fe0e4d71e173bd85975df828cb3ad2d38223e5dd
岩田峻 <shun15992@gmail.com>
2023-05-24 23:20:57 +0900

    完了と未完了の数表示

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import { useTodoList } from '/src/composables/useTodoList.js';
 const todoRef = ref('');
 const isEditRef = ref(false);
-const { todoListRef, add, show, edit, del, check } = useTodoList();
+const { todoListRef, add, show, edit, del, check, countFin } = useTodoList();
 
 const addTodo = () => {
   add(todoRef.value);
@@ -24,6 +24,10 @@ const deleteTodo = (id) => {
 };
 const changeCheck = (id) => {
   check(id);
+};
+const countFinMethod = () => {
+  const finArr = todoListRef.value.filter((todo) => !todo.checked);
+  return finArr.length;
 };
 </script>
 
@@ -53,6 +57,10 @@ const changeCheck = (id) => {
         <button class="btn pink" @click="deleteTodo(todo.id)">削</button>
       </div>
     </div>
+  </div>
+  <div class="finCount">
+    <span>完了：{{ countFin }}</span>
+    <span>未完了：{{ countFinMethod() }}</span>
   </div>
 </template>
 
@@ -116,5 +124,10 @@ const changeCheck = (id) => {
   text-decoration: line-through;
   background-color: #ddd;
   color: #777;
+}
+
+.finCount {
+  margin-top: 8px;
+  font-size: 0.8em;
 }
 </style>
